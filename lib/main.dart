6248e5b96343f8adc5f0703dd7ba4976acf0782f
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2023-08-14 17:35:32 +0500

    Implementing Custom Switch Filter button

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final materialScannerTheme = ScannerTheme();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Material Scanner",
       theme: materialScannerTheme.lightTheme(),
       darkTheme: materialScannerTheme.darkTheme(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,12 +14,69 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: true,
         centerTitle: false,
-        title: const Text("Your Documents"),
+        titleSpacing: 16,
+        title: const Text(
+          "Your Documents",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         actions: _buildAppBarTrailingIcon(),
       ),
       floatingActionButton: buildFloatingActionButton(),
       bottomNavigationBar: buildBottomNavigationBar(),
+      body: Column(
+        children: [
+          _buildSearchBarAndFilter(),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildSearchBarAndFilter() {
+    return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  constraints: const BoxConstraints(maxHeight: 46),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintText: "Search document, folder",
+                      contentPadding: const EdgeInsets.symmetric(vertical:10),
+                    ),
+
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                flex: 1,
+                child: _buildCustomFilterSwitcher(),
+              ),
+            ],
+          ),
+        );
+  }
+
+  Widget _buildCustomFilterSwitcher(){
+    return Container(
+      constraints: const BoxConstraints(maxHeight: 46),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: Theme.of(context).colorScheme.surfaceVariant,
+      ),
     );
   }
 
@@ -51,10 +108,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Widget> _buildAppBarTrailingIcon() {
     return [
-        IconButton.filledTonal(
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: IconButton.filledTonal(
+          padding: EdgeInsets.zero,
           icon: const Icon(Icons.create_new_folder_outlined),
           onPressed: () {},
         ),
-      ];
+      ),
+    ];
   }
 }
